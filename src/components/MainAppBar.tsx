79fce7b8b24263c239ea4e325304073eb3d11bda
secretshardul <aeershardul@gmail.com>
2023-10-11 23:31:38 +0400

    feat: Dark mode toggle button

--- src/components/MainAppBar.tsx
+++ src/components/MainAppBar.tsx
@@ -4,25 +4,29 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { ColorModeContext } from '../contexts/ColorModeContext';
 
 export default function MainAppBar() {
+  const { isDarkMode, toggleColorMode } = React.useContext(ColorModeContext);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             OpenBLE UI
           </Typography>
+
+          {/* Dark Theme Toggle Button */}
+          <IconButton color="inherit" onClick={toggleColorMode}>
+            {
+              isDarkMode
+                ? <Brightness7Icon />
+                : <Brightness4Icon />
+            }
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,33 @@ import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.tsx'
 import { BluetoothProvider } from './contexts/BluetoothContext.tsx';
 import { SnackbarProvider } from 'notistack';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { ColorModeContext, ColorModeProvider } from './contexts/ColorModeContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ColorModeProvider>
+      <ThemedApp />
+    </ColorModeProvider>
+  </React.StrictMode>,
+)
+
+function ThemedApp() {
+  const { isDarkMode } = React.useContext(ColorModeContext);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: isDarkMode ? 'dark' : 'light',
+    },
+  });
+
+  return <ThemeProvider theme={darkTheme}>
     <CssBaseline />
     <BluetoothProvider>
       <SnackbarProvider maxSnack={3}>
         <App />
       </SnackbarProvider>
     </BluetoothProvider>
-
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+}
