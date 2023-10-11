0c240a3ebb49322650d42871351b55e3fc66e5bc
secretshardul <aeershardul@gmail.com>
2023-10-11 19:46:53 +0400

    style: Remove card for services

--- src/components/ServiceComponent/index.tsx
+++ src/components/ServiceComponent/index.tsx
@@ -1,7 +1,5 @@
 import ListItem from '@mui/material/ListItem'
-import Card from '@mui/material/Card'
 import WarningIcon from '@mui/icons-material/Warning'
-import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Unstable_Grid2'
 import Chip from '@mui/material/Chip'
@@ -32,61 +30,58 @@ export default function ServiceComponent({
   const connectedService = connectedServices.get(serviceUuid)
 
   return <ListItem>
-    <Card sx={{ width: '100%' }}>
-      <CardContent>
-        <Grid container spacing={1} alignItems="baseline">
-          <Grid>
-            <Typography variant='h5'>{`${index + 1}. ${service.name}`}</Typography>
-          </Grid>
-          <Grid>
-            <Typography variant="subtitle1">{service.summary}</Typography>
-          </Grid>
+    <Box>
+      <Grid container spacing={1} alignItems="baseline">
+        <Grid>
+          <Typography variant='h5'>{`${index + 1}. ${service.name}`}</Typography>
         </Grid>
-        <Box marginTop={2} />
-        <Grid container spacing={1}>
-          <Grid direction="row">
-            <Typography><strong>UUID: </strong>{serviceUuid}</Typography>
-
-          </Grid>
-          <Grid>
-            <Chip label={service.source} color="primary" size="small" />
-          </Grid>
-          <Grid xs={12}>
-            <Typography><strong>Identifier: </strong>{service.identifier}</Typography>
-          </Grid>
-          {
-            bluetoothDevice !== undefined && !matchService(connectedService) &&
-              <Grid xs={12} marginTop={1}>
-                <Chip label="Failed to match" icon={<WarningIcon />} color="warning" />
-              </Grid>
-          }
+        <Grid>
+          <Typography variant="subtitle1">{service.summary}</Typography>
         </Grid>
+      </Grid>
+      <Box marginTop={2} />
+      <Grid container spacing={1}>
+        <Grid direction="row">
+          <Typography><strong>UUID: </strong>{serviceUuid}</Typography>
 
-        <Box marginTop={2} />
-        <Grid container spacing={1}>
-          <Grid>
-            <Typography variant="h6">Characteristics</Typography>
-          </Grid>
-          <Grid>
-            <Chip label={`#${Object.keys(service.characteristics).length}`} />
-          </Grid>
-          <Grid xs={12}>
-            <List>
-              {
-                Object.entries(service.characteristics).map(([characteristicUuid, characteristic], index) => {
-                  return <CharacteristicCard
-                    key={characteristicUuid}
-                    index={index}
-                    uuid={characteristicUuid}
-                    characteristic={characteristic}
-                  />
-                })
-              }
-            </List>
-          </Grid>
         </Grid>
+        <Grid>
+          <Chip label={service.source} color="primary" size="small" />
+        </Grid>
+        <Grid xs={12}>
+          <Typography><strong>Identifier: </strong>{service.identifier}</Typography>
+        </Grid>
+        {
+          bluetoothDevice !== undefined && !matchService(connectedService) &&
+          <Grid xs={12} marginTop={1}>
+            <Chip label="Failed to match" icon={<WarningIcon />} color="warning" />
+          </Grid>
+        }
+      </Grid>
 
-      </CardContent>
-    </Card>
+      <Box marginTop={2} />
+      <Grid container spacing={1}>
+        <Grid>
+          <Typography variant="h6">Characteristics</Typography>
+        </Grid>
+        <Grid>
+          <Chip label={`#${Object.keys(service.characteristics).length}`} />
+        </Grid>
+        <Grid xs={12}>
+          <List>
+            {
+              Object.entries(service.characteristics).map(([characteristicUuid, characteristic], index) => {
+                return <CharacteristicCard
+                  key={characteristicUuid}
+                  index={index}
+                  uuid={characteristicUuid}
+                  characteristic={characteristic}
+                />
+              })
+            }
+          </List>
+        </Grid>
+      </Grid>
+    </Box>
   </ListItem>
 }
